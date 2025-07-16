166b545b88f0b54c936b0a9ddc348148cd1228e8
Toshiharu-Konuma-sti <t-konuma@sios.com>
2025-07-16 19:36:40 +0900

    improved an implimentation by splitting functions

--- container/CREATE_CONTAINER.sh
+++ container/CREATE_CONTAINER.sh
@@ -1,32 +1,56 @@
 #!/bin/sh
 
-clear
-S_TIME=$(date +%s)
-CUR_DIR=$(cd $(dirname $0); pwd)
+# {{{ start_banner()
+start_banner()
+{
+	echo "############################################################"
+	echo "# START SCRIPT"
+	echo "############################################################"
+}
+# }}}
 
-echo "############################################################"
-echo "# START SCRIPT"
-echo "############################################################"
-
+# {{{ destory_container()
+destory_container()
+{
 echo "\n### START: Destory existing containers ##########"
 docker-compose \
 	-f docker-compose.yml \
 	-f docker-compose-app.yml \
 	down -v --remove-orphans
+}
+# }}}
 
-#	echo "\n### START: Install a plugin to collect container's logs ##########"
-#	docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions
+# {{{ install_plugin_collectiong_log()
+install_plugin_collectiong_log()
+{
+	echo "\n### START: Install a plugin to collect container's logs ##########"
+	docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions
+}
+# }}}
 
+# {{{ create_container()
+create_container()
+{
 echo "\n### START: Create new containers ##########"
 docker-compose \
 	-f docker-compose.yml \
 	-f docker-compose-app.yml \
 	up -d -V --remove-orphans
+}
+# }}}
 
-echo "\n### START: Show a list of container ##########"
-docker ps -a
+# {{{ show_list_container()
+show_list_container()
+{
+	echo "\n### START: Show a list of container ##########"
+	docker ps -a
+}
+# }}}
 
-cat << EOS
+# {{{ show_url()
+show_url()
+{
+	cat << EOS
 
 /************************************************************
  * Information:
@@ -54,11 +78,57 @@ cat << EOS
  ***********************************************************/
 
 EOS
+}
+# }}}
 
+# {{{ finish_banner()
+# $1: time to start this script
+finish_banner()
+{
+	S_TIME=$1
+	E_TIME=$(date +%s)
+	DURATION=$((E_TIME - S_TIME))
+	echo "############################################################"
+	echo "# FINISH SCRIPT ($DURATION seconds)"
+	echo "############################################################"
+}
+# }}}
 
-E_TIME=$(date +%s)
-DURATION=$((E_TIME - S_TIME))
+S_TIME=$(date +%s)
+CUR_DIR=$(cd $(dirname $0); pwd)
 
-echo "############################################################"
-echo "# FINISH SCRIPT ($DURATION seconds)"
-echo "############################################################"
+case "$1" in
+	"down")
+		clear
+		start_banner
+		destory_container
+		show_list_container
+		finish_banner $S_TIME
+		;;
+	"up")
+		clear
+		start_banner
+		create_container
+		show_list_container
+		show_url
+		finish_banner $S_TIME
+		;;
+	"list")
+		clear
+		show_list_container
+		;;
+	"")
+		clear
+		start_banner
+		destory_container
+		create_container
+		show_list_container
+		show_url
+		finish_banner $S_TIME
+		;;
+	*)
+		echo "Usage: $0 [down|up|list]"
+		echo ""
+		exit 1
+		;;
+esac
