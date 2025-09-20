9c36d5481fd2f0c83608d56b6968cb98ff0be0a2
Toshiharu-Konuma-sti <t-konuma@sios.com>
2025-09-20 14:02:09 +0900

    integrated the container and the setup functions

--- container/functions.sh
+++ container/functions.sh
@@ -22,6 +22,31 @@ finish_banner()
 # }}}
 
 
+# {{{ call_own_fname()
+call_own_fname()
+{
+	OFNM=$(basename $0)
+	echo "$OFNM"
+}
+# }}}
+
+# {{{ call_show_start_banner()
+# $0: the name of the script being executed 
+call_show_start_banner()
+{
+	echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n> START: Script = [$(call_own_fname)]\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+}
+# }}}
+
+# {{{ call_show_finish_banner()
+# $0: the name of the script being executed 
+call_show_finish_banner()
+{
+	echo "\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n< FINISH: Script = [$(call_own_fname)]\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+}
+# }}}
+
+
 # {{{ create_container()
 # $1: the current directory
 create_container()
